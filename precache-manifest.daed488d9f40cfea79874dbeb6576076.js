493d0029af5f121117ed79018ba99bc31a881c32
pacm4n83 <kemkeut@gmail.com>
2020-03-03 17:13:34 +0100

    Updates

---
+++ precache-manifest.daed488d9f40cfea79874dbeb6576076.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "325bf21b6c6971d33433",
+    "url": "/recosmo/static/js/runtime~main.325bf21b.js"
+  },
+  {
+    "revision": "0bdc7c86fd6b7de62c33",
+    "url": "/recosmo/static/js/main.0bdc7c86.chunk.js"
+  },
+  {
+    "revision": "29d9ce0c5a0bd8704685",
+    "url": "/recosmo/static/js/1.29d9ce0c.chunk.js"
+  },
+  {
+    "revision": "0bdc7c86fd6b7de62c33",
+    "url": "/recosmo/static/css/main.45bca33a.chunk.css"
+  },
+  {
+    "revision": "29d9ce0c5a0bd8704685",
+    "url": "/recosmo/static/css/1.9281de9d.chunk.css"
+  },
+  {
+    "revision": "57fd4f4c8cd7b57e73bf6ed22491d182",
+    "url": "/recosmo/index.html"
+  }
+];
